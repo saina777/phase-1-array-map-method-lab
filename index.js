0dae163f676ae5e3172ec4bb0f03efa1af8ff5c4
saina777 <dw0208090@gmail.com>
2025-06-29 23:42:09 +0300

    array

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+const titleCased = () => {  return tutorials.map(tutorial => {
+    return tutorial
+      .split(' ')
+      .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(' ');
+  });
+};
